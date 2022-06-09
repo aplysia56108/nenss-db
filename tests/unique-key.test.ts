e82fba19935e5c939ff28445629c216f6b282726
aplysia <56108keitaro@gmail.com>
2022-06-09 11:11:13 +0900

    delete test

--- tests/unique-key.test.ts
+++
@@ -1,7 +0,0 @@
-import UniqueKey from '../src/common/unique-key/index.js';
-
-test('generate', () => {
-  const name = 'abc';
-  const message = UniqueKey.generate();
-  expect(message).toBe(name);
-});
