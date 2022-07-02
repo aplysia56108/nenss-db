e31a4b2dc666c568490eea03a401830aaa664846
aplysia56108 <46865735+aplysia56108@users.noreply.github.com>
2022-07-02 14:06:29 +0900

    rewrite all commenting outs in JSdoc. fix import path.

--- src/iterator/i-iterator.ts
+++ src/iterator/i-iterator.ts
@@ -1,13 +1,45 @@
+import Leaf from 'src/leaf';
+
 interface IIterator<T, U> {
-  // decrement its index.
+  /**
+   * Increment its index.
+   */
   prev(): boolean;
-  // increment its index.
+  /**
+   * Decrement its index.
+   */
   next(): boolean;
-  // return the corresponding pair of key and item. if out og range, return a pair of null.
-  get(): [T, U] | [null, null];
-  // return the corresponding index.
+  /**
+   * Return the corresponding key. if out of range, throws error.
+   */
+  getKey(): T;
+  /**
+   * Return the corresponding item. if out of range, throws error.
+   */
+  getItem(): U;
+  /**
+   *  Return the corresponding index.
+   */
+  set(item: U): void;
+  /**
+   *  Return the leaf which the data is contained.
+   */
+  getLeaf(): Leaf<T, U>;
+  /**
+   *  Return the position in leaf.
+   */
+  getPosition(): number;
+  /**
+   *  Return whether it is a valied iterator.
+   */
+  isValid(): boolean;
+  /**
+   *  Return the corresponding index.
+   */
   toIndex(): number;
-  // initialize.
+  /**
+   *  Clear out.
+   */
   clear(): void;
 }
 
