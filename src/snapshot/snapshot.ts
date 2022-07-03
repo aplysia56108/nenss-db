2842aaf47e6d553ed894fe85ed4b682fb96cff39
aplysia <56108keitaro@gmail.com>
2022-07-03 17:36:24 +0900

    in snapshot change to wrap only InnerObject. null was excluded.

--- src/snapshot/snapshot.ts
+++ src/snapshot/snapshot.ts
@@ -9,17 +9,12 @@ type Data = number | string | boolean | { [key: string]: Data | null };
 class Snapshot<T = any> implements ISnapshot<T> {
   private object: Data | null;
   private isData: boolean;
-  constructor(innerObject: InnerObject | null) {
-    if (innerObject === null) {
-      this.object = null;
+  constructor(innerObject: InnerObject) {
+    this.object = DataConverter.toData(innerObject);
+    if (this.object === null) {
       this.isData = false;
     } else {
-      this.object = DataConverter.toData(innerObject);
-      if (this.object === null) {
-        this.isData = false;
-      } else {
-        this.isData = true;
-      }
+      this.isData = true;
     }
   }
 
