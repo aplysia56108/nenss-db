a5e45eef327cd3061220523685ab71e1fcf0bf5f
aplysia <56108keitaro@gmail.com>
2022-07-04 23:41:39 +0900

    fixed snapshot along with the change of toData.

--- tests/snapshot/snapshot.test.ts
+++ tests/snapshot/snapshot.test.ts
@@ -1,9 +1,16 @@
 import InnerObject from '../../src/inner-object';
+import { nullData } from '../../src/common/types';
 import { Snapshot } from '../../src/snapshot';
 
 test('null test', () => {
   const innerObject = new InnerObject({}, '', 5);
-  const snapshot = new Snapshot(innerObject);
+  const snapshot = new Snapshot(innerObject.getObject());
+  expect(snapshot.val()).toBe(null);
+  expect(snapshot.exists()).toBe(false);
+});
+
+test('null test2', () => {
+  const snapshot = new Snapshot(nullData);
   expect(snapshot.val()).toBe(null);
   expect(snapshot.exists()).toBe(false);
 });
@@ -11,7 +18,7 @@ test('null test', () => {
 test('value test', () => {
   const innerObject = new InnerObject({}, '', 5);
   innerObject.setObject(5);
-  const snapshot = new Snapshot(innerObject);
+  const snapshot = new Snapshot(innerObject.getObject());
   expect(snapshot.val()).toBe(5);
   expect(snapshot.exists()).toBe(true);
 });
