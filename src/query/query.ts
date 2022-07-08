af3270a1727b840934e5e22d1d93e7ac917a0c6c
aplysia <56108keitaro@gmail.com>
2022-07-08 16:11:31 +0900

    changed the type of status.

--- src/query/query.ts
+++ src/query/query.ts
@@ -1,12 +1,12 @@
 class Query {
   func: () => Promise<void>;
-  status: string;
+  status: 'read-or-write' | 'read' | 'write';
   id: string;
   ref: string;
   constructor(
     func: () => Promise<void>,
     ref: string,
-    status = 'read-or-write',
+    status: 'read-or-write' | 'read' | 'write' = 'read-or-write',
     id: string | null = null,
   ) {
     this.func = func;
